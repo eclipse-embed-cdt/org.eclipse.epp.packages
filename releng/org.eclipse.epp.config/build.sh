88030c2b528e8e4e850a86d429371bdcdda90fde
Markus Knauer <mknauer@eclipsesource.com>
2007-06-12 15:41:59 +0000

    first version for RC3 (Eclipse / Europa) using staging site snapshot 20070612

--- releng/org.eclipse.epp.config/build.sh
+++ releng/org.eclipse.epp.config/build.sh
@@ -8,22 +8,26 @@ cd /home/mknauer/packaging/eclipse
 # RCP
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseRCP_33M7.xml
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseRCP_33RC1.xml
-/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseRCP_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseRCP_33RC2.xml
+/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseRCP_33RC3.xml
 
 # CDT
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseCDT_33M7.xml
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseCDT_33RC1.xml
-/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseCDT_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseCDT_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseCDT_33RC3.xml
 
 # Java
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJava_33M7.xml
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJava_33RC1.xml
-/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJava_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJava_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJava_33RC3.xml
 
 # Java EE
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJavaEE_33M7.xml
 #/opt/java/jdk1.5.0_07.modified/bin/java -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJavaEE_33RC1.xml
-/opt/java/jdk1.5.0_07.modified/bin/java -Xms64m -Xmx512m -XX:MaxPermSize=128m -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJavaEE_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -Xms64m -Xmx512m -XX:MaxPermSize=128m -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJavaEE_33RC2.xml
+#/opt/java/jdk1.5.0_07.modified/bin/java -Xms64m -Xmx512m -XX:MaxPermSize=128m -jar startup.jar -consolelog -data /home/mknauer/packaging/target /home/mknauer/packaging/EclipseJavaEE_33RC3.xml
 
 
 #################################################
