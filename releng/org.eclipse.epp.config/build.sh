dde7d8fc8e2a2c561ed24684f16f5a6c5edf1586
Markus Knauer <mknauer@eclipsesource.com>
2007-06-16 18:32:48 +0000

    more updates

--- releng/org.eclipse.epp.config/build.sh
+++ releng/org.eclipse.epp.config/build.sh
@@ -4,19 +4,47 @@ DIRECTORY=$PWD
 
 cd $DIRECTORY/Eclipse_for_RCP_Plugin_Developers
 sh ./build.sh
+cd $DIRECTORY/Eclipse_for_RCP_Plugin_Developers
 sh ./build.sh
 
 cd $DIRECTORY/Eclipse_IDE_for_C_C++_Developers
 sh ./build.sh
+cd $DIRECTORY/Eclipse_IDE_for_C_C++_Developers
 sh ./build.sh
 
 cd $DIRECTORY/Eclipse_IDE_for_Java_Developers
 sh ./build.sh
+cd $DIRECTORY/Eclipse_IDE_for_Java_Developers
 sh ./build.sh
 
 cd $DIRECTORY/Eclipse_IDE_for_JEE_Developers
 sh ./build.sh
+cd $DIRECTORY/Eclipse_IDE_for_JEE_Developers
 sh ./build.sh
+
+mkdir $DIRECTORY/result
+find $DIRECTORY -name "epp-*" -maxdepth 3 -type f -exec mv {} $DIRECTORY/result/ \;
+cd $DIRECTORY/result
+
+mv epp-cpp-europa-linux.gtk.x86.tar.gz       epp-cpp-europa-linux-gtk.tar.gz
+mv epp-cpp-europa-macosx.carbon.x86.tar.gz   epp-cpp-europa-macosx-carbon.tar.gz
+mv epp-cpp-europa-win32.win32.x86.zip        epp-cpp-europa-win32.zip
+
+mv epp-rcp-europa-linux.gtk.x86.tar.gz       epp-rcp-europa-linux-gtk.tar.gz
+mv epp-rcp-europa-macosx.carbon.x86.tar.gz   epp-rcp-europa-macosx-carbon.tar.gz
+mv epp-rcp-europa-win32.win32.x86.zip        epp-rcp-europa-win32..zip
+
+mv epp-java-europa-linux.gtk.x86.tar.gz      epp-java-europa-linux-gtk.tar.gz
+mv epp-java-europa-macosx.carbon.x86.tar.gz  epp-java-europa-macosx-carbon.tar.gz
+mv epp-java-europa-win32.win32.x86.zip       epp-java-europa-win32.zip
+
+mv epp-jee-europa-linux.gtk.x86.tar.gz       epp-jee-europa-linux-gtk.tar.gz
+mv epp-jee-europa-macosx.carbon.x86.tar.gz   epp-jee-europa-macosx-carbon.tar.gz
+mv epp-jee-europa-win32.win32.x86.zip        epp-jee-europa-win32..zip
+
+for FILE in *[zp]; do md5sum -b $FILE >$FILE.md5; done
+for FILE in *[zp]; do sha1sum -b $FILE >$FILE.sha1; done
+
 
 
 #################################################
