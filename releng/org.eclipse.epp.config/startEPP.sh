d9c979c16eb1351dde90169414be50bbbb8fd3bc
Markus Knauer <mknauer@eclipsesource.com>
2007-12-14 16:05:25 +0000

    once again

--- releng/org.eclipse.epp.config/startEPP.sh
+++ releng/org.eclipse.epp.config/startEPP.sh
@@ -14,13 +14,29 @@ rm -r workspace*
 
 # check-out configuration
 cd $WORKING_DIR
-cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/technology -P checkout $PACKAGE
+cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/technology checkout -P $PACKAGE
 
 # build
+cd $ECLIPSE_DIR
+WORKSPACE=$WORKING_DIR/workspaceCDT
+mkdir $WORKSPACE
+./eclipse -data $WORKSPACE \
+    -consoleLog \
+    -vm $VM $WORKING_DIR/EclipseCDT_340.xml \
+    2>&1 1>$WORKSPACE/log.txt
+    
 cd $ECLIPSE_DIR
 WORKSPACE=$WORKING_DIR/workspaceJava
 mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
-    -vm $VM $WORKING_DIR/EclipseCDT_340.xml \
+    -vm $VM $WORKING_DIR/EclipseJava_340.xml \
+    2>&1 1>$WORKSPACE/log.txt
+
+cd $ECLIPSE_DIR
+WORKSPACE=$WORKING_DIR/workspaceRCP
+mkdir $WORKSPACE
+./eclipse -data $WORKSPACE \
+    -consoleLog \
+    -vm $VM $WORKING_DIR/EclipseRCP_340.xml \
     2>&1 1>$WORKSPACE/log.txt
