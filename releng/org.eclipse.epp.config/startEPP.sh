5cff86fd1107744c0f97450107bcbe46276d39d5
Markus Knauer <mknauer@eclipsesource.com>
2007-12-14 16:17:41 +0000

    update

--- releng/org.eclipse.epp.config/startEPP.sh
+++ releng/org.eclipse.epp.config/startEPP.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # variables
+START_TIME=`date -u +%Y%m%d%H%M%S`
 WORKING_DIR=$HOME/epp.build
 ECLIPSE_DIR=$WORKING_DIR/eclipse
 VM=$HOME/ibm-java2-ppc-50/jre/bin/java
@@ -11,6 +12,10 @@ PACKAGE=org.eclipse.epp/releng/org.eclipse.epp.config
 # remove old workspaces
 cd  $WORKING_DIR
 rm -r workspace*
+
+# create target directory
+TARGET_DIR=$WORKING_DIR/$START_TIME
+mkdir $TARGET_DIR
 
 # check-out configuration
 cd $WORKING_DIR
@@ -23,15 +28,21 @@ mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
     -vm $VM $WORKING_DIR/EclipseCDT_340.xml \
-    2>&1 1>$WORKSPACE/log.txt
-    
+    2>&1 1>$WORKSPACE/cdt.log
+cd $WORKSPACE
+for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
+mv $WORKSPACE/cdt.log $TARGET_DIR/
+
 cd $ECLIPSE_DIR
 WORKSPACE=$WORKING_DIR/workspaceJava
 mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
     -vm $VM $WORKING_DIR/EclipseJava_340.xml \
-    2>&1 1>$WORKSPACE/log.txt
+    2>&1 1>$WORKSPACE/java.log
+cd $WORKSPACE
+for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
+mv $WORKSPACE/java.log $TARGET_DIR/
 
 cd $ECLIPSE_DIR
 WORKSPACE=$WORKING_DIR/workspaceRCP
@@ -39,4 +50,9 @@ mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
     -vm $VM $WORKING_DIR/EclipseRCP_340.xml \
-    2>&1 1>$WORKSPACE/log.txt
+    2>&1 1>$WORKSPACE/rcp.log
+cd $WORKSPACE
+for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
+mv $WORKSPACE/rcp.log $TARGET_DIR/
+
+
