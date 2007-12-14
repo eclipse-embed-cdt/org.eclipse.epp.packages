7a39fcdee3dc49b8b5164d7f2c894534aa53401c
Markus Knauer <mknauer@eclipsesource.com>
2007-12-14 16:56:28 +0000

    new config check-in

--- releng/org.eclipse.epp.config/startEPP.sh
+++ releng/org.eclipse.epp.config/startEPP.sh
@@ -22,34 +22,48 @@ cd $WORKING_DIR
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/technology checkout -P $PACKAGE
 
 # build
+
+######### CDT
 cd $ECLIPSE_DIR
 WORKSPACE=$WORKING_DIR/workspaceCDT
 mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
-    -vm $VM $WORKING_DIR/EclipseCDT_340.xml \
+    -vm $VM $WORKING_DIR/$PACKAGE/Eclipse_IDE_for_C_C++_Developers/EclipseCDT_340.xml \
     2>&1 1>$TARGET_DIR/cdt.log
 cd $WORKSPACE
 for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
 
+######### Java
 cd $ECLIPSE_DIR
 WORKSPACE=$WORKING_DIR/workspaceJava
 mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
-    -vm $VM $WORKING_DIR/EclipseJava_340.xml \
+    -vm $VM $WORKING_DIR/$PACKAGE/Eclipse_IDE_for_Java_Developers/EclipseJava_340.xml \
     2>&1 1>$TARGET_DIR/java.log
 cd $WORKSPACE
 for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
 
+######### JEE
+cd $ECLIPSE_DIR
+WORKSPACE=$WORKING_DIR/workspaceJEE
+mkdir $WORKSPACE
+./eclipse -data $WORKSPACE \
+    -consoleLog \
+    -vm $VM $WORKING_DIR/$PACKAGE/Eclipse_IDE_for_JEE_Developers/EclipseJavaEE_340.xml \
+    2>&1 1>$TARGET_DIR/jee.log
+cd $WORKSPACE
+for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
+
+######### RCP
 cd $ECLIPSE_DIR
 WORKSPACE=$WORKING_DIR/workspaceRCP
 mkdir $WORKSPACE
 ./eclipse -data $WORKSPACE \
     -consoleLog \
-    -vm $VM $WORKING_DIR/EclipseRCP_340.xml \
+    -vm $VM $WORKING_DIR/$PACKAGE/Eclipse_for_RCP_Plugin_Developers/EclipseRCP_340.xml \
     2>&1 1>$TARGET_DIR/rcp.log
 cd $WORKSPACE
 for II in eclipse*; do mv $II $TARGET_DIR/$START_TIME\_$II; done
-
 
