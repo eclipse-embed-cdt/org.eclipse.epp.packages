b00b3f7a71d15997bf7aa8634ae018bfe64fe673
Markus Knauer <mknauer@eclipsesource.com>
2008-05-23 08:10:17 +0000

    reenabling p2build

--- releng/org.eclipse.epp.config/startEPP34.sh
+++ releng/org.eclipse.epp.config/startEPP34.sh
@@ -62,8 +62,7 @@ do
             -consoleLog \
             -vm ${VM} \
             ${PACKAGECONFIGURATION} \
-#            P2BUILD \
-#            -Dorg.eclipse.epp.p2director=${ECLIPSE_DIR}/eclipse \
+            P2BUILD \
             2>&1 1>${TARGET_DIR}/${PACKAGENAME}.log
     if [ $? = "0" ]; then
         echo -n "...successfully finished ${PACKAGENAME} package build"
