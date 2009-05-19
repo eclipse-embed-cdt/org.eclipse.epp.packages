38b80715f0da4f56cce66373f765e4fb74e988e3
Markus Knauer <mknauer@eclipsesource.com>
2009-05-19 15:42:21 +0000

    M7 -> RC1

--- releng/org.eclipse.epp.config/startEPP35.sh
+++ releng/org.eclipse.epp.config/startEPP35.sh
@@ -51,7 +51,7 @@ MARKERFILENAME=".epp.nightlybuild"
 STATUSFILENAME="status.stub"
 CVSPATH="org.eclipse.epp/releng/org.eclipse.epp.config"
 CVSPROJECTPATH="org.eclipse.epp/packages"
-RELEASE_NAME="-galileo-M7"
+RELEASE_NAME="-galileo-RC1"
 
 ###############################################################################
 
