22eeae7c5e226cea5926aba365da87b66d2322cf
Markus Knauer <mknauer@eclipsesource.com>
2009-06-14 09:13:22 +0000

    RC5 updates

--- releng/org.eclipse.epp.config/startEPP35.sh
+++ releng/org.eclipse.epp.config/startEPP35.sh
@@ -52,7 +52,7 @@ MARKERFILENAME=".epp.nightlybuild"
 STATUSFILENAME="status.stub"
 CVSPATH="org.eclipse.epp/releng/org.eclipse.epp.config"
 CVSPROJECTPATH="org.eclipse.epp/packages"
-RELEASE_NAME="-galileo-RC4"
+RELEASE_NAME="-galileo-RC5"
 
 ###############################################################################
 
