54d7fff9056a304cd2b30ac5133120d7f38b8a67
Markus Knauer <mknauer@eclipsesource.com>
2010-04-27 11:20:23 +0000

    * bulk changes for M7

--- releng/org.eclipse.epp.config/startEPP36.sh
+++ releng/org.eclipse.epp.config/startEPP36.sh
@@ -59,7 +59,7 @@ MARKERFILENAME=".epp.nightlybuild"
 STATUSFILENAME="status.stub"
 CVSPATH="org.eclipse.epp/releng/org.eclipse.epp.config"
 CVSPROJECTPATH="org.eclipse.epp/packages"
-RELEASE_NAME="-helios-M6"
+RELEASE_NAME="-helios-M7"
 
 ###############################################################################
 
