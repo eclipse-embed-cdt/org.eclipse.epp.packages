79717a03a464fe3a3abccd53b29be8d640c9fccd
Markus Knauer <mknauer@eclipsesource.com>
2010-08-17 08:47:53 +0000

    * all: update Helios SR1 RC2

--- releng/org.eclipse.epp.config/startEPP36.sh
+++ releng/org.eclipse.epp.config/startEPP36.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/36
-RELEASE_NAME="-helios-SR1-RC0"
+RELEASE_NAME="-helios-SR1-RC2"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
