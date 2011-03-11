3ca40c0d58ad0c156855a68dbf7008b3b5d9e670
Markus Knauer <mknauer@eclipsesource.com>
2011-03-11 13:41:16 +0000

    * M6 updates

--- releng/org.eclipse.epp.config/startEPPIndigo.sh
+++ releng/org.eclipse.epp.config/startEPPIndigo.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/indigo
-RELEASE_NAME="-indigo-M5"
+RELEASE_NAME="-indigo-M6"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
