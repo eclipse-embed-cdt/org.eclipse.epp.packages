7eb63c641360fe5c5b4326eef4e3f57f118b3214
Markus Knauer <mknauer@eclipsesource.com>
2011-05-21 14:43:18 +0000

    * RC1 -> RC2

--- releng/org.eclipse.epp.config/startEPPIndigo.sh
+++ releng/org.eclipse.epp.config/startEPPIndigo.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/indigo
-RELEASE_NAME="-indigo-RC1"
+RELEASE_NAME="-indigo-RC2"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
