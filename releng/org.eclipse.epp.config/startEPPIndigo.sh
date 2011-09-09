c4ee36e454f0aab97756b80cf22b02af18f07f05
Markus Knauer <mknauer@eclipsesource.com>
2011-09-09 18:18:54 +0000

    * all: RC4 updates

--- releng/org.eclipse.epp.config/startEPPIndigo.sh
+++ releng/org.eclipse.epp.config/startEPPIndigo.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/indigo
-RELEASE_NAME="-indigo-SR1-RC3"
+RELEASE_NAME="-indigo-SR1-RC4"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
