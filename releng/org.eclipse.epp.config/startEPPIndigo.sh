11c16cd7a78a61697a347e3ea9ffa589958e09a3
Markus Knauer <mknauer@eclipsesource.com>
2011-09-09 06:14:03 +0000

    * all: bug 356590 - About Dialog update for Indigo SR1 * all: SR1-RC3 updates

--- releng/org.eclipse.epp.config/startEPPIndigo.sh
+++ releng/org.eclipse.epp.config/startEPPIndigo.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/indigo
-RELEASE_NAME="-indigo-SR1-RC2"
+RELEASE_NAME="-indigo-SR1-RC3"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
