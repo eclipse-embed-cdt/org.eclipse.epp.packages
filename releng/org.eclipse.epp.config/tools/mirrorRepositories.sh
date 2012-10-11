38f00c8ab647df20f774f40f64710ff1b8411925
Markus Knauer <mknauer@eclipsesource.com>
2012-10-11 16:01:18 +0200

    Add custom bash script for mirroring p2 repositories

---
+++ releng/org.eclipse.epp.config/tools/mirrorRepositories.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# directory that points to an Eclipse SDK or similar
+ECLIPSE_DIR="/opt/eclipse/64/eclipse.rcp.juno"
+#ECLIPSE_DIR="/shared/technology/epp/epp_build/juno/eclipse"
+
+ECLIPSE="${ECLIPSE_DIR}/eclipse"
+
+if [ $# -gt "0" ]
+then
+  echo "Using list of given URLs."
+  until [ -z "$1" ]  # Until all parameters used up . . .
+    do
+      REPO_URL=${1}
+      echo "Using URL ${REPO_URL}"
+      shift
+      ${ECLIPSE} -noSplash -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication -source ${REPO_URL} -destination file:.
+      ${ECLIPSE} -noSplash -application org.eclipse.equinox.p2.metadata.repository.mirrorApplication -source ${REPO_URL} -destination file:.
+    done
+else
+  echo "Searching for local repositories."
+  for II in `ls -tr */content.jar`;
+    do
+      REPO_URL=${II%/content.jar}
+      echo "Using URL ${REPO_URL}"
+      ${ECLIPSE} -noSplash -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication -source ${REPO_URL} -destination file:.
+      ${ECLIPSE} -noSplash -application org.eclipse.equinox.p2.metadata.repository.mirrorApplication -source ${REPO_URL} -destination file:.
+    done
+fi  
+
