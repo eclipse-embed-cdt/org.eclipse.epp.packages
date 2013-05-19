5c4fd278a7dae0cea55375c1eaf51b1a71e09281
Markus Knauer <mknauer@eclipsesource.com>
2013-05-19 14:54:56 +0200

    Update scripts and XML files to Kepler RC1

--- releng/org.eclipse.epp.config/startEPPKepler.sh
+++ releng/org.eclipse.epp.config/startEPPKepler.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/kepler
-RELEASE_NAME="-kepler-M7"
+RELEASE_NAME="-kepler-RC1"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
