c7d9d7bec2a41d65471997101195e30d22a8b583
Markus Knauer <mknauer@eclipsesource.com>
2013-06-10 20:12:11 +0200

    Update website files and config to Kepler RC4

--- releng/org.eclipse.epp.config/startEPPKepler.sh
+++ releng/org.eclipse.epp.config/startEPPKepler.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/kepler
-RELEASE_NAME="-kepler-RC3"
+RELEASE_NAME="-kepler-RC4"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
