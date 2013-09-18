3d602e8727dd4ce00d9387848e3d13879af8972f
Markus Knauer <mknauer@eclipsesource.com>
2013-09-18 17:29:27 +0200

    Update version strings to Kepler SR1-RC4

--- releng/org.eclipse.epp.config/startEPPKepler.sh
+++ releng/org.eclipse.epp.config/startEPPKepler.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/kepler
-RELEASE_NAME="-kepler-SR1-RC3"
+RELEASE_NAME="-kepler-SR1-RC4"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
