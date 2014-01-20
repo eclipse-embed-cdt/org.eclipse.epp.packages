fbf68461ea042fff39331dbc6655ce63a07ede21
Markus Knauer <mknauer@eclipsesource.com>
2014-01-20 14:02:53 +0100

    Update EPP to Kepler SR2
    - Update package names to -kepler-SR2-RC1
    - Update EPP Keper SR2 versions to 2.0.2
    - Update copyrights in about dialog to 2014

--- releng/org.eclipse.epp.config/startEPPKepler.sh
+++ releng/org.eclipse.epp.config/startEPPKepler.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/kepler
-RELEASE_NAME="-kepler-SR1-RC4"
+RELEASE_NAME="-kepler-SR2-RC1"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
@@ -41,7 +41,7 @@ DOWNLOAD_BASE_URL="http://build.eclipse.org/technology/epp/epp_build/kepler/down
 DOWNLOAD_BASE_DIR="${BASE_DIR}/download"
 BUILD_DIR="${BASE_DIR}/build"
 DOWNLOAD_DIR="${DOWNLOAD_BASE_DIR}/${START_TIME}"
-EPPREPO_INPUT_DIR="/shared/technology/epp/epp_repo/kepler/epp.build/buildresult/org.eclipse.epp.allpackages.feature_2.0.1-eclipse.feature/site.p2"
+EPPREPO_INPUT_DIR="/shared/technology/epp/epp_repo/kepler/epp.build/buildresult/org.eclipse.epp.allpackages.feature_2.0.2-eclipse.feature/site.p2"
 EPPREPO_WORKINGCOPY_DIR="${DOWNLOAD_DIR}/repository"
 MARKERFILE="${DOWNLOAD_DIR}/${MARKERFILENAME}"
 STATUSFILE="${DOWNLOAD_DIR}/${STATUSFILENAME}"
