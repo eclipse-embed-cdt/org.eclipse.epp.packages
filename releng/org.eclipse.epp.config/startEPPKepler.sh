32afe5878c9fedaa7cb8baf7e12d25dc2881ab4c
Markus Knauer <mknauer@eclipsesource.com>
2014-02-10 09:34:20 +0100

    Update configuration to Kepler SR2-RC3

--- releng/org.eclipse.epp.config/startEPPKepler.sh
+++ releng/org.eclipse.epp.config/startEPPKepler.sh
@@ -26,7 +26,7 @@ fi
 
 # variables to adjust
 BASE_DIR=/shared/technology/epp/epp_build/kepler
-RELEASE_NAME="-kepler-SR2-RC2"
+RELEASE_NAME="-kepler-SR2-RC3"
 
 # variables
 START_TIME=`date -u +%Y%m%d-%H%M`
