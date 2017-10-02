a6f16dce54ed3991e6a2b150dcd9845893e9e4a1
Mickael Istria <mistria@redhat.com>
2017-10-02 15:08:35 +0200

    Bug 525426 - a Help > Contribute menu
    Signed-off-by: Mickael Istria <mistria@redhat.com>
    Change-Id: I0d7148722a7748eed9103a14540eddfa4f34c52b

---
+++ packages/org.eclipse.epp.package.common/src/org/eclipse/epp/common/ContributeHandler.java
@@ -0,0 +1,46 @@
+/****************************************************************************
+* Copyright (c) 2017 Red Hat Inc. and others
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
+package org.eclipse.epp.common;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.swt.program.Program;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ContributeHandler extends AbstractHandler {
+
+	private static final String CONTRIBUTE_URL = "https://www.eclipse.org/contribute/";
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+		if (activeWorkbenchWindow == null) {
+			return new Status(IStatus.ERROR, "org.eclipse.epp.package.common", "No active workbench window");
+		}
+		try {
+			IWebBrowser browser = activeWorkbenchWindow.getWorkbench().getBrowserSupport().createBrowser(getClass().getName());
+			browser.openURL(new URL(CONTRIBUTE_URL));
+			return Status.OK_STATUS;
+		} catch (PartInitException e) {
+			Program.launch(CONTRIBUTE_URL);
+			return new Status(IStatus.OK, "org.eclipse.epp.package.common", e.getMessage(), e);
+		} catch (MalformedURLException e) {
+			return new Status(IStatus.ERROR, "org.eclipse.epp.package.common", e.getMessage(), e);
+		}
+	}
+
+}
