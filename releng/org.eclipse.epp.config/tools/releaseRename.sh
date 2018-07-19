c620f26ac7cb69b5cc60c69f4ce3468623c2db4a
Markus Knauer <mknauer@eclipsesource.com>
2018-07-19 16:38:41 +0200

    Update strings and versions for 2018-09
    Carefully update occurrences of the old version 4.8 to 4.9, and from
    photon to 2018-09. There are still some references to photon that should
    be updated manually by the package maintainers, specifically the p2
    repository URLs in some of the packages that are still referencing a
    photon based URL.
    
    Change-Id: Ia29c96a232d2b3b50222ab6b1db3ace0742da3b0
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- releng/org.eclipse.epp.config/tools/releaseRename.sh
+++ releng/org.eclipse.epp.config/tools/releaseRename.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RELEASETRAIN=photon
+RELEASETRAIN=2018-09
 RELEASEDIRECTORY=/home/data/httpd/download.eclipse.org/technology/epp/downloads/release
 TESTDIRECTORY=/shared/technology/epp/epp_build/${RELEASETRAIN}/download
 CURRENTDIR=${PWD}
