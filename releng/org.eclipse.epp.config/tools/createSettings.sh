4a8cad6bfc5d1841e6e59c6a131fa67d7c6e4212
Ed Merks <ed.merks@gmail.com>
2019-09-01 07:15:39 +0200

    Bug 550628 - Migrate EPP to consistently use EPL 2.0 and SUA 2.0
    Change each about.html to the 2.0 version.
    Change all copyright headers in all file types with references to EPL
    2.0, including the copyright property in each feature.properties.
    Remove all epl-v10.html files and the one EPL-v2.0.html file; the
    appropriate license file is provided by the license feature resolution
    during the build.
    Remove references to the license file from the build.properties of each
    feature.
    Change each *.product file to embed a correct version of SUA 2.0
    
    Change-Id: I0c2507d10520a65ff7fc45d8e9ee7aecfcc4ac1b
    Signed-off-by: Ed Merks <ed.merks@gmail.com>

--- releng/org.eclipse.epp.config/tools/createSettings.sh
+++ releng/org.eclipse.epp.config/tools/createSettings.sh
@@ -2,9 +2,9 @@
 #
 #  Copyright (c) 2014, 2015 Innoopract Informationssysteme GmbH and others.
 #  All rights reserved. This program and the accompanying materials
-#  are made available under the terms of the Eclipse Public License v1.0
+#  are made available under the terms of the Eclipse Public License v2.0
 #  which accompanies this distribution, and is available at
-#  http://www.eclipse.org/legal/epl-v10.html
+#  http://www.eclipse.org/legal/epl-v20.html
 # 
 #  Contributors:
 #     Innoopract Informationssysteme GmbH - initial API and implementation
